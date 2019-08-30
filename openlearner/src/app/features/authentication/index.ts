615599769c726947d098bfb3f4464656aacfb3a4
Nogret Humphrey <nogrethumphrey@gmail.com>
2019-08-30 11:02:44 +0200

    remove all files angular files

--- openlearner/src/app/features/authentication/index.ts
+++
@@ -1,8 +0,0 @@
-export { AuthService, LoginOpt } from './auth.service';
-export { AuthModule } from './auth.module';
-export { SocialUser } from './entities/user';
-export { GoogleLoginProvider } from './providers/google-login-provider';
-export { FacebookLoginProvider } from './providers/facebook-login-provider';
-export { LinkedInLoginProvider } from './providers/linkedIn-login-provider';
-export { AuthServiceConfig } from './auth.service';
-export { AuthServiceConfigItem} from './auth.service';
