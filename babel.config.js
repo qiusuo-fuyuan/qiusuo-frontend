86e82f7614617abba7533d9447a10c647ca8f415
Ryan Zheng <ryan.zheng@unic.com>
2019-09-19 10:47:29 +0200

    setup the structure build structure of FE

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "plugins": [
+      "@babel/plugin-transform-runtime",
+      "macros"
+    ],
+    "presets": [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@lingui/babel-preset-react",
+      //"@babel/preset-typescript"
+    ],
+    "sourceType":"unambiguous"
+  }
